28abdce81401ab14a5d66a9f2b4f15d64af92da3
Francisco Calveyra <franciscocalveyra24@gmail.com>
2024-04-30 12:17:58 -0300

    Spotless Applied, autograding working Last commit :) 🔴🔵🙏 🤌🟣

--- engine/src/main/java/edu/austral/dissis/chess/utils/ChessMove.java
+++ engine/src/main/java/edu/austral/dissis/chess/utils/ChessMove.java
@@ -1,10 +1,43 @@
 package edu.austral.dissis.chess.utils;
 
+import java.util.Objects;
+
 import static edu.austral.dissis.chess.utils.ChessPosition.toAlgebraic;
 
-public record ChessMove(ChessPosition from, ChessPosition to) {
-  @Override
-  public String toString() {
-    return toAlgebraic(from) + " -> " + toAlgebraic(to);
-  }
+public final class ChessMove {
+    private final ChessPosition from;
+    private final ChessPosition to;
+
+    public ChessMove(ChessPosition from, ChessPosition to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return toAlgebraic(from) + " -> " + toAlgebraic(to);
+    }
+
+    public ChessPosition from() {
+        return from;
+    }
+
+    public ChessPosition to() {
+        return to;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ChessMove) obj;
+        return Objects.equals(this.from, that.from) &&
+                Objects.equals(this.to, that.to);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(from, to);
+    }
+
 }
--- engine/src/main/java/edu/austral/dissis/chess/utils/Pair.java
+++ engine/src/main/java/edu/austral/dissis/chess/utils/Pair.java
@@ -1,3 +1,42 @@
 package edu.austral.dissis.chess.utils;
 
-public record Pair<T, W>(T first, W second) {}
+import java.util.Objects;
+
+public final class Pair<T, W> {
+    private final T first;
+    private final W second;
+
+    public Pair(T first, W second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T first() {
+        return first;
+    }
+
+    public W second() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Pair) obj;
+        return Objects.equals(this.first, that.first) &&
+                Objects.equals(this.second, that.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair[" +
+                "first=" + first + ", " +
+                "second=" + second + ']';
+    }
+}
