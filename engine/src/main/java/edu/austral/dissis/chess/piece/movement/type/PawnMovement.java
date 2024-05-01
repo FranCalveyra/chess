7201c59440b810f4b53d5615e824f39e7bd8d40a
FranC-prog <franciscocalveyra24@gmail.com>
2024-05-01 20:29:07 -0300

    PawnMovement modularized

--- engine/src/main/java/edu/austral/dissis/chess/piece/movement/type/PawnMovement.java
+++ engine/src/main/java/edu/austral/dissis/chess/piece/movement/type/PawnMovement.java
@@ -5,26 +5,30 @@ import static edu.austral.dissis.chess.utils.MoveType.VERTICAL;
 import edu.austral.dissis.chess.engine.Board;
 import edu.austral.dissis.chess.piece.Piece;
 import edu.austral.dissis.chess.piece.movement.PieceMovement;
+import edu.austral.dissis.chess.piece.movement.restrictions.*;
 import edu.austral.dissis.chess.utils.ChessMove;
 import edu.austral.dissis.chess.utils.ChessPosition;
+import edu.austral.dissis.chess.validators.AndRestrictionValidator;
+import edu.austral.dissis.chess.validators.MovementRestrictionValidator;
+import edu.austral.dissis.chess.validators.OrRestrictionValidator;
 import edu.austral.dissis.chess.validators.PiecePathValidator;
 import java.awt.Color;
 
 public class PawnMovement implements PieceMovement {
-
   @Override
   public boolean isValidMove(ChessMove move, Board context) {
-    //TODO: change to validator
-    ChessPosition oldPos = move.from();
-    ChessPosition newPos = move.to();
-    int deltaY = newPos.getRow() - oldPos.getRow();
-    Piece currentPawn = context.pieceAt(oldPos);
-    boolean clearTile = context.pieceAt(newPos) == null;
-    boolean movementByColor =
-        currentPawn.getPieceColour() == Color.BLACK ? deltaY == -1 : deltaY == 1;
-    return (oldPos.getColumn() == newPos.getColumn()
-        && movementByColor
-        && new PiecePathValidator().isNoPieceBetween(oldPos, newPos, context, VERTICAL)
-        && clearTile);
+    Color team = context.pieceAt(move.from()).getPieceColour();
+    MovementRestrictionValidator validator = getPawnRestrictions(team);
+    return validator.isValidMove(move,context);
   }
+
+  private MovementRestrictionValidator getPawnRestrictions(Color team){
+    MovementRestriction colorBasedMovement = team == Color.BLACK ? new RowDistance(-1) : new RowDistance(1);
+    MovementRestrictionValidator dy = new AndRestrictionValidator(colorBasedMovement);
+    MovementRestrictionValidator dx = new AndRestrictionValidator(new AbsColumnDistance(0));
+    MovementRestrictionValidator movement = new AndRestrictionValidator(new NoPieceInPath(VERTICAL),dy,dx);
+    return new AndRestrictionValidator(new ClearTile(), movement,null);
+  }
+
+
 }
