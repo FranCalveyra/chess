66c4acd772a8e5b07989ac3297a1b43629cc09b7
Francisco Calveyra <franciscocalveyra24@gmail.com>
2024-05-07 17:02:10 -0300

    Added CheckersGame, modified PieceType enums, added CHECKER

---
+++ engine/src/main/java/edu/austral/dissis/checkers/engine/CheckersGame.java
@@ -0,0 +1,14 @@
+package edu.austral.dissis.checkers.engine;
+
+import edu.austral.dissis.common.engine.BoardGame;
+import edu.austral.dissis.common.utils.move.GameMove;
+import edu.austral.dissis.common.utils.result.GameResult;
+
+public class CheckersGame implements BoardGame {
+
+
+    @Override
+    public GameResult makeMove(GameMove move) {
+        return null;
+    }
+}
--- engine/src/main/java/edu/austral/dissis/common/piece/PieceType.java
+++ engine/src/main/java/edu/austral/dissis/common/piece/PieceType.java
@@ -9,5 +9,6 @@ public enum PieceType {
   BISHOP,
   ARCHBISHOP,
   CHANCELLOR,
+  CHECKER,
   ANY
 }
